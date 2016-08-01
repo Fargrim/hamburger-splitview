18fe25f7ffd559fd10a999527a33330a608d5d97
Kyle Blake <kyle.blake84@gmail.com>
2016-08-01 14:06:02 -0500

    Initial commit

---
+++ app/styles/index.js
@@ -0,0 +1,17 @@
+var styles = {
+  backgroundImg: {
+    backgroundImage: 'url("../app/images/pattern.svg")',
+    backgroundColor: '#ffa31a',
+    backgroundSize: 'cover',
+    height: '100%',
+    boxSizing: 'border-box'
+  },
+  space: {
+    marginTop: '25px'
+  },
+  backgroundColor: {
+    backgroundColor: 'rgba(252, 90, 44, 0.89)'
+  }
+};
+
+module.exports = styles;
