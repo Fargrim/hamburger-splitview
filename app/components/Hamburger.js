48fa83dac53009b325c85588c31c670b5725204c
Kyle Blake <kyle.blake84@gmail.com>
2016-08-03 17:07:54 -0500

    Trying to wrap my head around this.  Totally unfunctional right now.

--- app/components/Hamburger.js
+++ app/components/Hamburger.js
@@ -1,36 +1,70 @@
 import React, {Component} from 'react';
 import MenuSidebar from './MenuSidebar';
+import Rx from 'rxjs';
 
-class Hamburger extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-      content: [
-        {
-          image: 'app/images/ic_home_black_18dp_2x.png',
-          title: 'Home'
-        },
-        {
-          image: 'app/images/ic_star_rate_black_18dp_2x.png',
-          title: 'Favorites'
-        },
-        {
-          image: 'app/images/ic_settings_black_18dp_2x.png',
-          title: 'Settings'
-        },
-      ]
-    };
-  }
-  render() {
-    console.log('Hamburger open?', this.state.open);
-    return (
-      <div>
-        <button onClick={() => this.setState({open: !this.state.open})}><img src="app/images/ic_menu_black_24dp_2x.png" /></button>
-        <MenuSidebar menuOpen={this.state.open} content={this.state.content}/>
-      </div>
-    );
-  }
+
+const intent = (click$, props) => {
+  return Rx.Observable.fromEvent('.hamburger-menu', 'click');
 }
+const model = (click$, props) => {
+  return click$.map(() => ({
+    content: props.content
+  }));
+}
+const view = (state$) => {
+  const vtree$ = state$.map(state => {
+    return (
+    <div className="hamburger-menu">
+      <img src="app/images/ic_menu_black_24dp_2x.png" />
+      <MenuSidebar menuOpen={state.open} content={state.content}/>
+    </div>
+  )});
+  return vtree$;
+}
+
+
+const Hamburger = (props) => {
+  // const proxyClick$ = new Rx.Subject();
+  console.log(props);
+  const click$ = intent(props);
+  const state$ = model(click$);
+  const vtree$ = view(state$);
+  // Ditch the MenuSideBar for now.  Render it explicitly here instead.
+  return (
+    <div className="hamburger-menu">
+      <img src="app/images/ic_menu_black_24dp_2x.png" />
+    </div>
+  );
+}
+// class Hamburger extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       open: false,
+//       content: [
+//         {
+//           image: 'app/images/ic_home_black_18dp_2x.png',
+//           title: 'Home'
+//         },
+//         {
+//           image: 'app/images/ic_star_rate_black_18dp_2x.png',
+//           title: 'Favorites'
+//         },
+//         {
+//           image: 'app/images/ic_settings_black_18dp_2x.png',
+//           title: 'Settings'
+//         },
+//       ]
+//     };
+//   }
+//   render() {
+//     const proxyClick$ = new Rx.Subject();
+//     const click$ = intent(proxyClick$);
+//     const state$ = model(click$);
+//     const vtree$ = view(state$);
+//     // Ditch the MenuSideBar for now.  Render it explicitly here instead.
+//     return vtree$;
+//   }
+// }
 
 export default Hamburger;
--- app/index.js
+++ app/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hamburger from './components/Hamburger';
+import Rx from 'rxjs';
+
+const menu = {
+  content: [
+    {
+      image: 'app/images/ic_home_black_18dp_2x.png',
+      title: 'Home'
+    },
+    {
+      image: 'app/images/ic_star_rate_black_18dp_2x.png',
+      title: 'Favorites'
+    },
+    {
+      image: 'app/images/ic_settings_black_18dp_2x.png',
+      title: 'Settings'
+    },
+  ]
+};
+
+const intent = () => {
+  return Rx.Observable.fromEvent('.hamburger-menu', 'click');
+}
+const model = (click$, props) => {
+  return click$.map(() => ({
+    content: props.content
+  }));
+}
+const view = (state$) => {
+  const vtree$ = state$.map(state => {
+    return (
+    <div className="hamburger-menu">
+      <img src="app/images/ic_menu_black_24dp_2x.png" />
+      {state.content};
+    </div>
+  )});
+  return vtree$;
+}
+
+
+const Main = (props) => {
+  // const proxyClick$ = new Rx.Subject();
+  console.log(props);
+  const click$ = intent();
+  const state$ = model(click$);
+  const vtree$ = view(state$);
+  vtree$.map(vtree => console.log(vtree));
+  // Ditch the MenuSideBar for now.  Render it explicitly here instead.
+  return (
+    <div className="hamburger-menu">
+      <img src="app/images/ic_menu_black_24dp_2x.png" />
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <Hamburger/>,
+  Main(menu),
   document.getElementById('app')
 );
